a763230cf9a13e9e5ecbc389cb023962bf39d6e7
Your Name <you@example.com>
2019-06-10 19:37:11 +0800

    修改配置

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -151,7 +151,7 @@ class Index
             $image_url = 'http://easywechat.szbchm.com'.trim($hechengname,'.');
             trace('图片链接地址',$image_url);
             //$mediaIdres = $app->media->uploadImage($image_url);
-            $result = $app->material->uploadImage($image_url);
+            $result = $app->material->uploadImage($hechengname);
             trace('上传素材返回信息',json_encode($result));
             return new Image($result['media_id']);
 
