1438a8af94d4535bfcb3cb96a51e07b185520ca8
Your Name <you@example.com>
2019-06-10 15:49:29 +0800

    修改配置

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -89,7 +89,8 @@ class Index
                 return '上报地理位置事件';
                 break;
             case 'CLICK':  //自定义菜单事件  点击菜单拉取消息时的事件推送
-                return '自定义菜单事件  点击菜单拉取消息时的事件推送';
+                //return '自定义菜单事件  点击菜单拉取消息时的事件推送';
+                return $this->sendMessage($message); //推送带参数的二维码图文消息
                 break;
             case 'VIEW':   //自定义菜单事件  点击菜单跳转链接时的事件推送
                 return '自定义菜单事件  点击菜单跳转链接时的事件推送';
@@ -97,6 +98,54 @@ class Index
             default:
                 return '收到其它消息';
                 break;
+        }
+
+    }
+
+    //推送带参数的二维码图文消息
+    public function sendMessage($message)
+    {
+        if($message['EventKey'] == 'V1001_TODAY_MUSIC'){  //一元购点击事件
+            $app = app('wechat.official_account');
+            $result = $app->qrcode->temporary($message['FromUserName'], 6 * 24 * 3600);
+            trace('获取带参数二维码',json_encode($result));
+            $url = $app->qrcode->url($result['ticket']);
+
+            $content = file_get_contents($url); // 得到二进制图片内容
+
+            $path = './static/wechat_img/'.date('Ymd');
+            trace('图片保存路径',$path);
+            if(!file_exists($path))
+            {
+                //检查是否有该文件夹，如果没有就创建，并给予最高权限
+                mkdir($path, 0777);
+            }
+
+            $filename = $path.'/qr'.$message['FromUserName'].'.jpg';
+            trace('文件保存路径',$filename);
+            //将带参数的二维码保存到服务器
+            $res = file_put_contents($filename, $content); // 写入文件
+            trace('文件写入返回值1',$res);
+
+            //将微信用户图片保存到本地
+            //获取用户信息
+            $user = $app->user->get($message['FromUserName']);
+            $headimgcontent = file_get_contents($user['headimgurl']); // 得到二进制图片内容
+            $headfilename = $path.'/headimg'.$message['FromUserName'].'.jpg';
+            //将该用户微信图片保存到服务器
+            $res2 = file_put_contents($headfilename, $headimgcontent); // 写入文件
+            trace('文件写入返回值2',$res2);
+
+            // Array
+            // (
+            //     [ticket] => gQFD8TwAAAAAAAAAAS5odHRwOi8vd2VpeGluLnFxLmNvbS9xLzAyTmFjVTRWU3ViUE8xR1N4ajFwMWsAAgS2uItZAwQA6QcA
+            //     [expire_seconds] => 518400
+            //     [url] => http://weixin.qq.com/q/02NacU4VSubPO1GSxj1p1k
+            // )
+        }elseif ($message['EventKey'] == 'V1001_GOOD'){ //赞一下我们点击事件
+
+        }else{
+            return '未知点击事件';
         }
 
     }
