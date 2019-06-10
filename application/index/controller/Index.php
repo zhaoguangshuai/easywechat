894029888a477acc7bcafe2ee46ede8a54521233
Your Name <you@example.com>
2019-06-10 17:40:58 +0800

    修改配置

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -39,7 +39,7 @@ class Index
                     break;
                 case 'text':
                     //return '收到文字消息';
-                    new Text('收到文字消息');
+                    new Text('收到文字消息11');
                     break;
                 case 'image':
                     return '收到图片消息';
