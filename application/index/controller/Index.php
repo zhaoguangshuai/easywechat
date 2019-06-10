e275dddcc8ac406deea15764cea15bd07cbfc401
Your Name <you@example.com>
2019-06-10 20:42:01 +0800

    修改配置

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -155,9 +155,6 @@ class Index
             $result = $app->material->uploadImage($hechengname);
             //$result = $app->media->uploadImage($hechengname);
             trace('上传素材返回信息',json_encode($result));
-            trace('上传素材返回media_id',$result['media_id']);
-            $res = new Image('6Y0ORPyd40WcARxy5vkmFxDcjzVYNpoE3hxuffTKWeA');
-            trace('图片消息回复返回值',json_encode($res));
             return $result['media_id'];
 
             /*
